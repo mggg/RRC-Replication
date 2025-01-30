0b1eaa64e851abba4f2091e0966ee541bf5eed3c
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2025-01-29 18:49:27 -0700

    Update the multigrid scripts and folder structure

--- other_data_files/script_files/run_frcw_multigrid.sh
+++ other_data_files/script_files/run_frcw_multigrid.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
+# Get the directory of the current script
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+TOP_DIR=$(realpath "$SCRIPT_DIR/../..")
+
 echo "Running FRCW"
 
-frcw --graph-json ./JSON/square_multigraph.json \
-    --n-steps 1000000000 \
+frcw --graph-json "${TOP_DIR}/JSON_dualgraphs/square_multigrid.json" \
+    --n-steps 100000000 \
     --n-threads 10 \
     --pop-col TOTPOP \
-    --rng-seed 42 \
-    --tol 0.00001 \
+    --rng-seed 1000 \
+    --tol 0.0 \
     --batch-size 12 \
     --variant reversible \
     --assignment-col assignment \
     --cut-edges-count \
     --balance-ub 40 \
-    --writer ben -o square_rev_1B_2.jsonl.ben &
+    --writer ben -o "${TOP_DIR}/other_data_files/raw_data_files/square_multigrid/square_rev_100M.jsonl.ben" &
 
-frcw --graph-json ./JSON/linear_multigraph.json \
-    --n-steps 2000000000 \
+frcw --graph-json "${TOP_DIR}/JSON_dualgraphs/linear_multigrid.json" \
+    --n-steps 200000000 \
     --n-threads 10 \
     --pop-col TOTPOP \
-    --rng-seed 42 \
-    --tol 0.00001 \
+    --rng-seed 1000 \
+    --tol 0.0 \
     --batch-size 12 \
     --variant reversible \
     --assignment-col assignment \
     --cut-edges-count \
     --balance-ub 40 \
-    --writer ben -o linear_rev_2B.jsonl.ben &
+    --writer ben -o "${TOP_DIR}/other_data_files/raw_data_files/linear_multigrid/linear_rev_200M.jsonl.ben" &
 
 
 vars=("cut-edges-rmst" "district-pairs-rmst" "cut-edges-ust" "district-pairs-ust") 
@@ -34,29 +38,29 @@ var_names=("A" "B" "C" "D")
 
 for i in {0..3}
 do
-    frcw --graph-json ./JSON/square_multigraph.json \
-        --n-steps 10000000 \
+    frcw --graph-json "${TOP_DIR}/JSON_dualgraphs/square_multigrid.json" \
+        --n-steps 1000000 \
         --n-threads 1 \
         --pop-col TOTPOP \
         --rng-seed 1000 \
-        --tol 0.00001 \
+        --tol 0.0 \
         --batch-size 1 \
         --variant ${vars[$i]} \
         --assignment-col assignment \
-        --writer ben -o "square_${var_names[$i]}_10M.jsonl.ben" &
+        --writer ben -o "${TOP_DIR}/other_data_files/raw_data_files/square_multigrid/square_${var_names[$i]}_1M.jsonl.ben" &
 
-    echo "Done with square $i"
 
-    frcw --graph-json ./JSON/linear_multigraph.json \
-        --n-steps 10000000 \
+    frcw --graph-json "${TOP_DIR}/JSON_dualgraphs/linear_multigrid.json" \
+        --n-steps 1000000 \
         --n-threads 1 \
         --pop-col TOTPOP \
         --rng-seed 1000 \
-        --tol 0.00001 \
+        --tol 0.0 \
         --batch-size 1 \
         --variant ${vars[$i]} \
         --assignment-col assignment \
-        --writer ben -o "linear_${var_names[$i]}_10M.jsonl.ben" &
-
-    echo "Done with linear $i"
+        --writer ben -o "${TOP_DIR}/other_data_files/raw_data_files/linear_multigrid/linear_${var_names[$i]}_1M.jsonl.ben" &
 done 
+
+wait
+echo "Done!"
